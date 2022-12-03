f023c6a85434b1df7a4f73f80df13239216d411a
karlsonru <ivpolovnikov@hotmail.com>
2022-12-03 13:22:42 +0300

    feat: add streams transform solution

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,18 @@
+//transform.js - implement function that reads data from process.stdin, reverses text using Transform Stream and then writes it into process.stdout 
+import { stdin, stdout } from 'node:process';
+import { pipeline, Transform } from 'node:stream';
+import { createWriteStream, createReadStream } from 'node:fs';
+
 const transform = async () => {
-    // Write your code here 
+    pipeline(
+        stdin,
+        new Transform({
+            transform(chunk, encoding, callback) {
+                callback(null, String(chunk).split('').reverse().join('') + '\n');
+            },
+        }),
+        stdout,
+        (err) => { console.log('failed with error: ' + err) });
 };
 
 await transform();
